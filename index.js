2af8d9859eab84672873c2e4b735fd3f04f7ef52
tonny009 <tabassum.islam009@gmail.com>
2022-10-25 14:35:54 +0900

    Dynamically route declare

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const cors = require('cors');
 app.use(cors());
 const port = process.env.PORT || 5000;
 
-const categories = require('./Data/categories.json')
+const courses = require('./Data/categories.json')
 
 app.get('/', (req, res) => {
     res.send("Assignment API Running")
 })
 
-app.get('/news-categories', (req, res) => {
-    res.send(categories)
+app.get('/new-courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/course-details/:id', (req, res) => {
+    const id = req.params.id;
+    const selectCourse = courses.filter(c => c.id === id);
+    res.send(selectCourse)
 })
 
 app.listen(port, () => {
